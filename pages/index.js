ffe4c2a2824e7afc66daa5a7d9997c6466fdcdc7
Gabriel Formigoni <bielformigoni@gmail.com>
2022-11-21 16:25:43 -0300

    Conectando o front com o sanity

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,24 @@
 import React from "react";
+import { HeroBanner, Product, FooterBanner } from "../components";
 
 const Home = () => {
   return (
     <>
-      HeroBanner
-      <div>
+      <HeroBanner />
+      <div className="products-heading">
         <h2>Best selling products</h2>
         <p>
           Wireless headphones!! Get rid of wires, as well as all the problems to
           keep them organized and whole.
         </p>
       </div>
-      <div>
+      <div className="products-container">
         {["product 1", "product 2", "product 3"].map((product, index) => (
           <div key={index}>{product}</div>
         ))}
       </div>
+
+      <FooterBanner />
     </>
   );
 };
