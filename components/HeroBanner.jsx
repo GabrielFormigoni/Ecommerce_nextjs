0e10f671e199f5478c80a6b6b8323ad0e8d17d45
Gabriel Formigoni <bielformigoni@gmail.com>
2022-11-21 16:15:55 -0300

    Atualizando o HeroBanner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Link from "next/link"
 
 const Herobanner = () => {
   return (
-    <div>Herobanner</div>
+    <div className='hero-banner-container'>
+      <div>
+        <p>Small Text</p>
+        <h3>Mid text</h3>
+        <img src="" alt="headphones" className='hero-banner-image'/>
+
+        <div>
+          <Link href="/product/ID">
+            <button type='button'>Button text</button>
+          </Link>
+          <div className='desc'>
+            <h5>Description</h5>
+            <p>More Description</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
